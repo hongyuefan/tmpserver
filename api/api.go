3a610891dae8be920137f7fd181cd510125fa211
范洪月 <fanhongyue@zhongan.io>
2019-01-17 15:16:53 +0800

    asdf

--- api/api.go
+++ api/api.go
@@ -53,7 +53,7 @@ func (h *Handlers) HandlerAddMoney(c *gin.Context) {
 	if block, err = ethscan.GetLastBlock(); err != nil {
 		log.GetLog().LogWrite("GetLastBlock Error,Uid:", record.UID, "Address:", record.Address, "Hash:", record.Hash, "Money:", record.Money)
 	} else {
-		record.CheckedBlock = block - 3
+		record.CheckedBlock = block - 1
 	}
 
 	if _, err = models.AddRecord(&record); err != nil {
